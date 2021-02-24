e450080902d7e651b9a9f47df9c4bef5ffb46311
Artem <60553304+Artem7898@users.noreply.github.com>
2021-02-24 13:56:25 +0300

    Add files via upload

---
+++ test_basics/utils.py
@@ -0,0 +1,46 @@
+import yaml
+import json
+import time
+
+
+
+def get_addr_port(argv, config_file):
+    if len(argv) == 5:
+        port = argv[4]
+        addr = argv[2]
+    elif len(argv) == 3:
+        if argv[1] == '-p':
+            port = argv[2]
+            addr = parse_configs(config_file)[0]
+        elif argv[1] == '-a':
+            addr = argv[2]
+            port = parse_configs(config_file)[1]
+    else:
+        port = parse_configs(config_file)[1]
+        addr = parse_configs(config_file)[0]
+
+    return addr, port
+
+
+
+def parse_configs(file_name):
+    with open(file_name, encoding='utf-8') as y:
+        y_content = yaml.load(y, Loader=yaml.Loader)
+    return y_content['addr'], y_content['port']
+
+
+
+def get_msg(msg_in):
+    json_in = json.loads(msg_in)
+    return json_in
+
+
+def send_msg(destination_host, msg):
+    g = json.dumps(msg)
+    destination_host.send(g.encode('utf-8'))
+
+
+timestr = time.ctime(time.time()) + "\n"
+
+
+
